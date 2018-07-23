e088858065f8eee59081ea9d037544238d7171bf
fshibasaki <f-shibasaki@holon.cp.jp>
2018-07-23 11:03:23 +0900

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -22,14 +22,10 @@ class WebhookController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
         end
       end
     }
-    "OK"
+    render :nothing => true, status: :ok
   end
 
   #Lineからのcallbackか認証
