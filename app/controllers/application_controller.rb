dbfcfde0b410328b7dd3664c7fe831268dd5b72c
fshibasaki <f-shibasaki@holon.cp.jp>
2018-07-23 10:03:49 +0900

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ require 'line/bot'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  before_action :validate_signature
+ # before_action :validate_signature
 
-  def validate_signature
-    body = request.body.read
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
-    end
-  end
+ # def validate_signature
+ #   body = request.body.read
+ #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+ #   unless client.validate_signature(body, signature)
+ #     error 400 do 'Bad Request' end
+ #   end
+ # end
 
   def client
     @client ||= Line::Bot::Client.new { |config|
