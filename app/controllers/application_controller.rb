4cdb97bc2769cac86d39ea4fb1a527bfa9808020
fshibasaki <f-shibasaki@holon.cp.jp>
2018-07-23 10:14:17 +0900

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 require 'line/bot'
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+ # protect_from_forgery with: :null_session
 
  # before_action :validate_signature
 
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::Base
  #   end
  # end
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = "066a0e3810610a05f319b2a4b9537d7f"
-      config.channel_token = "cAGZX+n1ST0Bcz0SJ3LCXjDIgoOaM73lG5kdEE9k8ZIU2ZOtsmAKm8BEiHiFPo2KWosC8tkSvzSv2VvJ3UZJ0R9tTI1wkf0l66WFiEYF78Kb7/aX5+VP9sV8RGz0MP7WVG3YJhxjMk9jJ/8F7Vm2/wdB04t89/1O/w1cDnyilFU="
-    }
-  end
+ # def client
+ #   @client ||= Line::Bot::Client.new { |config|
+ #     config.channel_secret = "066a0e3810610a05f319b2a4b9537d7f"
+ #     config.channel_token = "cAGZX+n1ST0Bcz0SJ3LCXjDIgoOaM73lG5kdEE9k8ZIU2ZOtsmAKm8BEiHiFPo2KWosC8tkSvzSv2VvJ3UZJ0R9tTI1wkf0l66WFiEYF78Kb7/aX5+VP9sV8RGz0MP7WVG3YJhxjMk9jJ/8F7Vm2/wdB04t89/1O/w1cDnyilFU="
+ #   }
+ # end
 end
