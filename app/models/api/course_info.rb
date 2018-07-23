7bdc82f82609995137e4b4f6370e8c1e42e07aa2
fshibasaki <f-shibasaki@holon.cp.jp>
2018-07-23 15:06:27 +0900

    flex

--- app/models/api/course_info.rb
+++ app/models/api/course_info.rb
@@ -3,44 +3,79 @@ module Api
 
     def self.return_course_info(course, url)
       if course =~ /^東京芝$/
-        contents = {
-          type: 'carousel',
-          contents: [
-            {
-              type: 'bubble',
-              hero: {
-                type: 'image',
-                url: url + '/assets/course/tokyo-turf-2000.jpg',
-                size: 'full'
-              },
-              body: {
-                type: 'box',
-                layout: 'vertical',
-                contents: [
-                  {
-                    type: 'text',
-                    text: '東京 芝 2000m',
-                    size: 'xl',
-                    weight: 'bold',
-                    wrap: true
-                  },
-                  {
-                    type: 'text',
-                    text: 'クラスが上がるにつれ、スローで流れても逃げ残りが難しくなり差しが決まりやすい。\n 多頭数の場合は外枠不利。',
-                    size: 'sm',
-                    weight: 'bold',
-                    wrap: true
-                  }
-                ]
-              },
-              styles: {
-                hero: {
-                  backgroudColor: '#ffffff'
+       # contents = {
+       #   type: 'carousel',
+       #   contents: [
+       #     {
+       #       type: 'bubble',
+       #       hero: {
+       #         type: 'image',
+       #         url: url + '/assets/course/tokyo-turf-2000.jpg',
+       #         size: 'full'
+       #       },
+       #       body: {
+       #         type: 'box',
+       #         layout: 'vertical',
+       #         contents: [
+       #           {
+       #             type: 'text',
+       #             text: '東京 芝 2000m',
+       #             size: 'xl',
+       #             weight: 'bold',
+       #             wrap: true
+       #           },
+       #           {
+       #             type: 'text',
+       #             text: 'クラスが上がるにつれ、スローで流れても逃げ残りが難しくなり差しが決まりやすくなる。\n 多頭数の場合は外枠不利。',
+       #             size: 'sm',
+       #             weight: 'bold',
+       #             wrap: true
+       #           }
+       #         ]
+       #       },
+       #       styles: {
+       #         hero: {
+       #           backgroudColor: '#ffffff'
+       #         }
+       #       }
+       #     }
+       #   ]
+       # }
+        contents = [
+          {
+            type: 'bubble',
+            hero: {
+              type: 'image',
+              url: url + '/assets/course/tokyo-turf-2000.jpg',
+              size: 'full'
+            },
+            body: {
+              type: 'box',
+              layout: 'vertical',
+              contents: [
+                {
+                  type: 'text',
+                  text: '東京 芝 2000m',
+                  size: 'xl',
+                  weight: 'bold',
+                  wrap: true
+                },
+                {
+                  type: 'text',
+                  text: 'クラスが上がるにつれ、スローで流れても逃げ残りが難しくなり差しが決まりやすくなる。\n 多頭数の場合は外枠不利。',
+                  size: 'sm',
+                  weight: 'bold',
+                  wrap: true
                 }
+              ]
+            },
+            styles: {
+              hero: {
+                backgroudColor: '#ffffff'
               }
             }
-          ]
-        }
+          }
+        ]
       end
     end
 
