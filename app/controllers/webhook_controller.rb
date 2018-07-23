8ef612b41a4862027019da1b63d20d6299ef844c
fshibasaki <f-shibasaki@holon.cp.jp>
2018-07-23 11:14:22 +0900

    modify

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,5 @@
 class WebhookController < ApplicationController
+  protect_from_forgery except: :callback
 
   CHANNEL_SECRET = '066a0e3810610a05f319b2a4b9537d7f'
   CHANNEL_ACCESS_TOKEN = 'cAGZX+n1ST0Bcz0SJ3LCXjDIgoOaM73lG5kdEE9k8ZIU2ZOtsmAKm8BEiHiFPo2KWosC8tkSvzSv2VvJ3UZJ0R9tTI1wkf0l66WFiEYF78Kb7/aX5+VP9sV8RGz0MP7WVG3YJhxjMk9jJ/8F7Vm2/wdB04t89/1O/w1cDnyilFU='
@@ -25,12 +26,10 @@ class WebhookController < ApplicationController
         end
       end
     }
-    #render :nothing => true, status: :ok
     head :ok
   end
 
   #Lineからのcallbackか認証
-  #protect_from_forgery with: :null_session
 
  # CHANNEL_SECRET = '066a0e3810610a05f319b2a4b9537d7f'
  # #OUTBOUND_PROXY = 'http://fixie:VIyppzCnwQLL4wF@velodrome.usefixie.com:80'
