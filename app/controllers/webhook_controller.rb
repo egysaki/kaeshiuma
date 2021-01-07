60e4ed6d7d3712ed0737e34352bd4748828244a2
f-shibasaki <f-shibasaki@holon.co.jp>
2021-01-07 15:21:41 +0900

    hello world用にテスト

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -41,47 +41,12 @@ class WebhookController < ApplicationController
         case event.type
         when Line::Bot::Event::MessageType::Text
           url = root_url(only_path: false)
-          if event.message['text'] =~ /芝$|ダート$/
-            contents = Api::CourseInfo.return_course_info(event.message['text'], url)
-            if contents
-              message = {
-                type: 'flex',
-                altText: '競馬場コース特徴を表示',
-                contents: contents
-              }
-            else
-              message = {
-                type: 'text',
-                text: '該当するコースが見つかりませんでした。'
-              }
-            end
-         # elsif Horse.find_by(name: event.message['text'].gsub(/\d+| /, ''))
-         #   horse = Horse.find_by(name: event.message['text'].gsub(/\d+| /, ''))
-         #   limit = event.message['text'].slice(/\w+/).to_i
-         #   contents = Api::HorseInfo.return_horse_info(horse, url, limit)
-          else
-            horse_name = event.message['text'].gsub(/\d+| |[一-龠々]/, '')
-            course_designed = event.message['text'].match(/[一-龠々]+/).to_s
-            limit = event.message['text'].slice(/\w+/).to_i
-            contents = Api::ActiveHorseInfo.return_horse_info(horse_name, url, course_designed, limit)
-            if contents
-              message = {
-                type: 'flex',
-                altText: '競走馬の情報を表示',
-                contents: contents
-              }
-            else
-              message = {
-                type: 'text',
-                text: '該当する競走馬が見つかりませんでした。'
-              }
-            end
-         # else
-         #   message = {
-         #     type: 'text',
-         #     text: '未対応のコマンドです。'
-         #   }
-          end
+
+          message = {
+            type: 'text',
+            text: 'Hello World'
+          }
+
           response = client.reply_message(event['replyToken'], message)
           puts response
         end
@@ -89,6 +54,74 @@ class WebhookController < ApplicationController
     end
     head :ok
   end
+
+  #def callback
+  #  unless is_validate_signature
+  #    head 470
+  #  end
+
+  #  client = Line::Bot::Client.new { |config|
+  #    config.channel_secret = CHANNEL_SECRET
+  #    config.channel_token = CHANNEL_ACCESS_TOKEN
+  #  }
+
+  #  body = request.body.read
+  #  events = client.parse_events_from(body)
+
+  #  events.each do |event|
+  #    case event 
+  #    when Line::Bot::Event::Message
+  #      case event.type
+  #      when Line::Bot::Event::MessageType::Text
+  #        url = root_url(only_path: false)
+  #        if event.message['text'] =~ /芝$|ダート$/
+  #          contents = Api::CourseInfo.return_course_info(event.message['text'], url)
+  #          if contents
+  #            message = {
+  #              type: 'flex',
+  #              altText: '競馬場コース特徴を表示',
+  #              contents: contents
+  #            }
+  #          else
+  #            message = {
+  #              type: 'text',
+  #              text: '該当するコースが見つかりませんでした。'
+  #            }
+  #          end
+  #       # elsif Horse.find_by(name: event.message['text'].gsub(/\d+| /, ''))
+  #       #   horse = Horse.find_by(name: event.message['text'].gsub(/\d+| /, ''))
+  #       #   limit = event.message['text'].slice(/\w+/).to_i
+  #       #   contents = Api::HorseInfo.return_horse_info(horse, url, limit)
+  #        else
+  #          horse_name = event.message['text'].gsub(/\d+| |[一-龠々]/, '')
+  #          course_designed = event.message['text'].match(/[一-龠々]+/).to_s
+  #          limit = event.message['text'].slice(/\w+/).to_i
+  #          contents = Api::ActiveHorseInfo.return_horse_info(horse_name, url, course_designed, limit)
+  #          if contents
+  #            message = {
+  #              type: 'flex',
+  #              altText: '競走馬の情報を表示',
+  #              contents: contents
+  #            }
+  #          else
+  #            message = {
+  #              type: 'text',
+  #              text: '該当する競走馬が見つかりませんでした。'
+  #            }
+  #          end
+  #       # else
+  #       #   message = {
+  #       #     type: 'text',
+  #       #     text: '未対応のコマンドです。'
+  #       #   }
+  #        end
+  #        response = client.reply_message(event['replyToken'], message)
+  #        puts response
+  #      end
+  #    end
+  #  end
+  #  head :ok
+  #end
 
   private
 
